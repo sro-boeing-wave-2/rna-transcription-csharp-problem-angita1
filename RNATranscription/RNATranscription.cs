08ce1325892adb47e84d6ec48e3e93160829cad4
Yaiphabee <ksh.yaipha@gmail.com>
2018-07-20 15:59:58 +0530

    add rna file

--- RNATranscription/RNATranscription.cs
+++ RNATranscription/RNATranscription.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Concurrent;
+using System.Linq;
 
 namespace RNATranscriptionModule
 {
@@ -6,6 +8,28 @@ namespace RNATranscriptionModule
     {
         public static string ToRna(string nucleotide)
         {
+            string rna = "";        
+            foreach (char item in nucleotide)
+            {
+                switch (item)
+                {
+                    case 'G':
+                        rna += 'C';
+                        break;
+                    case 'C':
+                        rna += 'G';
+                        break;
+                    case 'T':
+                        rna += 'A';
+                        break;
+                    case 'A':
+                        rna += 'U';
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return rna;
             throw new NotImplementedException("You need to implement this function.");
         }
     }
